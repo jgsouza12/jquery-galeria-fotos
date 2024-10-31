331fb161cbf622899e96af2a5617502ab2368614
jgsouza12 <142275277+jgsouza12@users.noreply.github.com>
2024-10-31 09:33:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    
     $('header button').click(function(){
         $('form').slideDown();
-    })
+    });
 
-    $('botao-cancelar').click(function(){
+    $('#botao-cancelar').click(function(){
         $('form').slideUp();
-    })
+    });
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val;
-        const novoItem = $('<li style="display:none"></li>');
-        $(`<img src="$(enderecoDaNovaImagem)" />`).appendTo(novoItem);
+        const enderecoNovaImg = $(`#link-nova-img`).val();
+        
+        const novoItem = $('<li style="display: none;"></li>');
+        $(`<img src="${enderecoNovaImg}" />`).appendTo(novoItem);
         $(`
-            <div class="overlay-imagem-link">
-                <a href="$(enderecoDaNovaImagem)" target="_blank" title="ver imagem em tamnanho real"
-                    Ver imagem em tamanho real
+            <div class="overlay-link">
+                <a href="${enderecoNovaImg}" title='Ver a imagem em tamanho Real' target="_blank">Ver a imagem em tamanho Real</a>
             </div>
-            `).appendTo(novoItem);
+        `).appendTo(novoItem);
+        
         $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn( );
-        $('endereço-imagem-nova').val('')
-    })
-})
+        $(novoItem).fadeIn(800);
+        
+        $('#link-nova-img').val('');
+    });
+});
